b799970c6125b947f0287fd4f2ddd9ffb387a3e9
Daud <daudarslan869@gmail.com>
2022-12-18 16:57:51 +0500

    modify App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,7 @@ function App(props) {
 
         <div className="py-16 px-8 sm:px-20 md:px-28 lg:px-40">
           <Routes>
-            <Route
-              exact
-              path="/Text-Utils-App-Using-React"
-              element={<TextForm />}
-            />
+            <Route path="/" element={<TextForm />} />
             <Route path="about" element={<About />} />
           </Routes>
         </div>
